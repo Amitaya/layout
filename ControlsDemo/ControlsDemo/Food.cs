f9e8fdce2c3685a59108cd0168c88429c6638fd0
wanjie <865933005@qq.com>
2020-03-27 11:46:28 +0800

    demo for relativelayout Biduma

---
+++ ControlsDemo/ControlsDemo/Food.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ControlsDemo
+{
+    public class Food
+    {
+        public string ImageURL { 
+            get {
+                return "https://cdn.pixabay.com/photo/2018/08/20/17/04/wedding-3619423_1280.png";
+            } set { } }
+
+        public string Name { set; get; }
+
+        public string Type { set; get; }
+    }
+}
